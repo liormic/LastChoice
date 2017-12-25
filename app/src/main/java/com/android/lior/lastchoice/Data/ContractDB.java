ef7825b0b9947669cb5e7b26eb1880ccb097bbcd
Liormmm <liormm86@gmail.com>
2017-12-26 00:22:25 +0200

    First Commit

---
+++ app/src/main/java/com/android/lior/lastchoice/Data/ContractDB.java
@@ -0,0 +1,24 @@
+package com.android.lior.lastchoice.Data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by lior on 12/25/17.
+ */
+
+public class ContractDB   {
+
+    private ContractDB(){};
+
+
+    public class MovieData implements BaseColumns{
+
+        public static final String TABLE_NAME="Movies";
+        public static final String COLUMN_MOVIENAME="moviename";
+        public static final String COLUMN_Description="description";
+        public static final String COLUMN_YOUTUBEURL="url";
+        public static final String COLUMN_MOVIEIMAGE="image";
+
+
+    }
+}
